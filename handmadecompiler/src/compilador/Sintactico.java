0c845233102de85c35e7afa1c8612e2674025fce
ValentinPardo <dronesdetandil@gmail.com>
2024-10-04 11:08:15 -0300

    pequeño cambio de gramatica y pruebas de parser

--- handmadecompiler/src/compilador/Sintactico.java
+++ handmadecompiler/src/compilador/Sintactico.java
@@ -1,5 +1,7 @@
 package compilador;
 
+import java.util.ArrayList;
+
 public class Sintactico {
     private static volatile Sintactico unicaInstancia = new Sintactico(); 
     private Sintactico() {}
@@ -8,15 +10,18 @@ public class Sintactico {
     	return unicaInstancia;
     }
 
-    public void ejecutar(AnalizadorLexico lexico) {
+    public ArrayList<Integer> ejecutar(AnalizadorLexico lexico) {
         boolean flag = true;
+        ArrayList<Integer> listaDeTokens = new ArrayList<Integer>();
         Par par;
         while (flag) {
             par = lexico.getProximoToken();
+            listaDeTokens.add(par.getId());
             if (par.getId() == 0)
                 flag = false;
         }
-        
+        System.out.println("Lista de tokens: " + listaDeTokens);
+        return listaDeTokens;
     }
     
 }
